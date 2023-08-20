94d58984fdd144c74ace2e9bd9a34cb13b5f945a
krisbobo <kiyongbohbo@gmail.com>
2023-08-21 00:01:58 +0100

    Add logic to solve challenge

--- challenge.rb
+++ challenge.rb
@@ -2,5 +2,13 @@ def stock_picker(arr)
   buying_day = []
   selling_day = []
   best_days = []
-
+arr.forEach do |i|
+    if i < i + 1
+      buying_day.push(i)
+    elsif i > i + 1
+      selling_day.push(i)
+    else
+      best_days.push(i)
+    end
+  end
 end
