6408623beb253d5931a5e9b7ce3f256d2cec61f2
krisbobo <kiyongbohbo@gmail.com>
2023-08-27 23:58:59 +0100

    Restart challenge

--- challenge.rb
+++ challenge.rb
@@ -1,16 +1,16 @@
 def stock_picker(arr)
-  buying_day = []
-  selling_day = []
-  best_days = []
-arr.forEach do |i|
-    if i < i + 1
-      buying_day.push(i)
-    elsif i > i + 1
-      selling_day.push(i)
-    else
-      best_days.push(i)
-    end
-  end
+#   buying_day = []
+#   selling_day = []
+#   best_days = []
+# arr.forEach do |i|
+#     if i < i + 1
+#       buying_day.push(i)
+#     elsif i > i + 1
+#       selling_day.push(i)
+#     else
+#       best_days.push(i)
+#     end
+#   end
 end
 
 puts stock_picker([17,3,6,9,15,8,6,1,10])
