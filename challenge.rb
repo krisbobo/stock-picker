1f7cbcee60114e547a7dfcce26aef5bb004ffeb2
krisbobo <kiyongbohbo@gmail.com>
2023-09-12 23:57:26 +0100

    Remove logic

--- challenge.rb
+++ challenge.rb
@@ -2,15 +2,15 @@ def stock_picker(arr)
   buying_day = []
   selling_day = []
   best_days = []
-  for i in arr 
-    if i < i + 1
-      buying_day.push(i)
-    elsif i > i + 1
-      selling_day.push(i)
-    else
-      best_days.push(i)
-    end
-  end
+  # for i in arr 
+  #   if i < i + 1
+  #     buying_day.push(i)
+  #   elsif i > i + 1
+  #     selling_day.push(i)
+  #   else
+  #     best_days.push(i)
+  #   end
+  # end
 end
 
 # puts stock_picker([17,3,6,9,15,8,6,1,10])
