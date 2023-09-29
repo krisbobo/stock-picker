c5f2fe41fb6253c614d802695b5b6b77c6ac445f
krisbobo <kiyongbohbo@gmail.com>
2023-09-29 23:59:14 +0100

    Add final solution

--- challenge.rb
+++ challenge.rb
@@ -1,16 +1,17 @@
-def stock_picker(arr)
-  buying_day = []
-  selling_day = []
-  best_days = []
-  for i in arr 
-    if i < i + 1
-      buying_day.push(i)
-    elsif i > i + 1
-      selling_day.push(i)
-    else
-      best_days.push(i)
+def stock_picker(prices)
+  max_profit = 0
+  buy, sell = 0, 0
+
+  prices.each_with_index do |buy_price, buy_day|
+    prices[buy_day + 1 ..-1].each_with_index do |sell_price, sell_day|
+      if sell_price - buy_price > max_profit
+        max_profit = sell_price - buy_price
+        buy, sell = buy_day, sell_day + buy_day + 1
+      end
     end
   end
+
+  max_profit > 0 ? [buy, sell] : nil
 end
 
-# puts stock_picker([17,3,6,9,15,8,6,1,10])
+puts stock_picker([17,3,6,9,15,8,6,1,10])
